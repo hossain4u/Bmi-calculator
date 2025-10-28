fc439eb99ab027a993a316948f5a7fbd654413af
hossain4u <sujonmazi20@gmail.com>
2025-10-28 07:49:14 +0600

    update bmi calculator project

--- bmi_calculator/lib/home_screen.dart
+++ bmi_calculator/lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-enum HeightType {cm, feetInch}
+
+enum HeightType { cm, feetInch }
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,50 +16,68 @@ class _HomeScreenState extends State<HomeScreen> {
   final cmCtr = TextEditingController();
   final feetCtr = TextEditingController();
   final inchCtr = TextEditingController();
-  String _bmiResult= "";
+  String _bmiResult = "";
+  String? category;
 
-  double? cmToM(){
-    final cm =double.tryParse(cmCtr.text.trim());
-    if (cm == null || cm <=0 ) return null;
-
-    return cm/100.0;
+  String categoryResult(double bmi) {
+    if (bmi < 18.5) return "Underweight";
+    if (bmi < 24.9) return "Healthy Weight";
+    if (bmi < 29.9) return "Overweight";
+    return "Obesity";
   }
-  double? feetInchToM(){
-    final feet =double.tryParse(feetCtr.text.trim());
-    final inch =double.tryParse(inchCtr.text.trim());
 
-    if (feet == null || feet <=0 || inch == null || inch <=0 ){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('Invalid Value')));
+  double? cmToM() {
+    final cm = double.tryParse(cmCtr.text.trim());
+    if (cm == null || cm <= 0) return null;
+
+    return cm / 100.0;
+  }
+
+  double? feetInchToM() {
+    final feet = double.tryParse(feetCtr.text.trim());
+    final inch = double.tryParse(inchCtr.text.trim());
+
+    if (feet == null || feet <= 0 || inch == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Invalid Value')));
       return null;
     }
-    final totalInch= feet*12+inch;
-    if(totalInch <=0){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('Invalid Value')));
+    final totalInch = feet * 12 + inch;
+    if (totalInch <= 0) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Invalid Value')));
       return null;
     }
     return totalInch * 0.0254;
   }
 
-  void _calculator(){
-    final weight =double.tryParse(weightCtr.text.trim());
-    if (weight == null || weight <=0 ){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('Invalid Value')));
+  void _calculator() {
+    final weight = double.tryParse(weightCtr.text.trim());
+    if (weight == null || weight <= 0) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Invalid Value')));
       return;
     }
     final m = heightType == HeightType.cm ? cmToM() : feetInchToM();
-    if (m== null){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('Invalid Value')));
+    if (m == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Invalid Value')));
       return;
     }
-    final bmi = weight /(m*m);
+    final bmi = weight / (m * m);
+    final cat = categoryResult(bmi);
     setState(() {
       _bmiResult = bmi.toStringAsFixed(2);
+      category = cat;
     });
-
   }
 
   @override
-  void dispose(){
+  void dispose() {
     weightCtr.dispose();
     cmCtr.dispose();
     feetCtr.dispose();
@@ -66,13 +85,10 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
-@override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: Text('BMI Calculator'), centerTitle: true),
       body: ListView(
         padding: EdgeInsets.all(15),
         children: [
@@ -80,62 +96,78 @@ class _HomeScreenState extends State<HomeScreen> {
             controller: weightCtr,
             decoration: InputDecoration(
               labelText: "Weight (Kg)",
-              border: OutlineInputBorder(
-              )
+              border: OutlineInputBorder(),
             ),
           ),
-          const SizedBox(height: 15,),
-          Text('Height Unit',style: TextStyle(fontSize:20, fontWeight: FontWeight.bold),),
-          const SizedBox(height: 12,),
-          SegmentedButton<HeightType>(segments: [
-            const ButtonSegment<HeightType>(value: HeightType.cm, label: Text("cm")),
-            const ButtonSegment<HeightType>(value: HeightType.feetInch, label: Text("Feet/Inch")),
-          ], selected: {heightType},
-          onSelectionChanged: (value)=> setState(()=> heightType =value.first ),
+          const SizedBox(height: 15),
+          Text(
+            'Height Unit',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 12),
+          SegmentedButton<HeightType>(
+            segments: [
+              const ButtonSegment<HeightType>(
+                value: HeightType.cm,
+                label: Text("cm"),
+              ),
+              const ButtonSegment<HeightType>(
+                value: HeightType.feetInch,
+                label: Text("Feet/Inch"),
+              ),
+            ],
+            selected: {heightType},
+            onSelectionChanged: (value) =>
+                setState(() => heightType = value.first),
           ),
           const SizedBox(height: 10),
-          if(heightType == HeightType.cm)...[
+          if (heightType == HeightType.cm) ...[
             TextFormField(
               controller: cmCtr,
               decoration: InputDecoration(
-                  labelText: "Height (Cm)",
-                  border: OutlineInputBorder(
-                  )
+                labelText: "Height (Cm)",
+                border: OutlineInputBorder(),
               ),
             ),
-          ]else...[
+          ] else ...[
             Row(
               children: [
                 Expanded(
                   child: TextFormField(
                     controller: feetCtr,
                     decoration: InputDecoration(
-                        labelText: "Feet(')",
-                        border: OutlineInputBorder(
-                        )
+                      labelText: "Feet(')",
+                      border: OutlineInputBorder(),
                     ),
                   ),
                 ),
-                const SizedBox(width:10),
+                const SizedBox(width: 10),
                 Expanded(
                   child: TextFormField(
                     controller: inchCtr,
                     decoration: InputDecoration(
-                        labelText: 'Inch(")',
-                        border: OutlineInputBorder(
-                        )
+                      labelText: 'Inch(")',
+                      border: OutlineInputBorder(),
                     ),
                   ),
                 ),
               ],
-            )
+            ),
           ],
-          const SizedBox(height: 15,),
+          const SizedBox(height: 15),
           ElevatedButton(onPressed: _calculator, child: Text("Show Result")),
-          const SizedBox(height: 15,),
-          Text("Result : $_bmiResult"),
-          const SizedBox(height: 15,),
-
+          const SizedBox(height: 15),
+          Card(
+            child: ListTile(
+              title: Text(
+                "Result : $_bmiResult",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          SizedBox(height: 8),
+          Card(child: ListTile(title: Text("Category : $category", style:TextStyle(fontSize: 20),))),
+          const SizedBox(height: 15),
         ],
       ),
     );
